34a4e4a07809846517d6aadc5e3d0330395824bb
r-francisco <rfrancisco@laughlin.com>
2018-11-24 15:15:53 -0600

    Array change, conver to list after response
    API didn't have a base id for the results.  Adding a level to target the results in anything that uses the api.

--- src/main/java/com/rf/searchapi/service/JokeServiceImpl.java
+++ src/main/java/com/rf/searchapi/service/JokeServiceImpl.java
@@ -1,8 +1,13 @@
 package com.rf.searchapi.service;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.rf.searchapi.model.JokeResult;
 import com.rf.searchapi.model.JokeResultList;
 
 @Service("jokeService")
@@ -15,7 +20,12 @@ public class JokeServiceImpl implements JokeService {
 	@Override
 	public JokeResultList getJokes() {
 		RestTemplate template = new RestTemplate();
-		return template.getForObject(apiBase, JokeResultList.class);		
+		List<JokeResult> finalList = Arrays.asList(template.getForObject(apiBase, JokeResult[].class));
+		
+		JokeResultList allTheJokes = new JokeResultList();
+		allTheJokes.setResults(finalList);
+		
+		return allTheJokes;		
 	}
 
 }
