104e49301b6c0a69734a58bf14df08b2af5b0584
r-francisco <rfrancisco@laughlin.com>
2018-11-24 14:42:05 -0600

    Updating to call API

--- src/main/java/com/rf/searchapi/service/JokeServiceImpl.java
+++ src/main/java/com/rf/searchapi/service/JokeServiceImpl.java
@@ -1,15 +1,20 @@
 package com.rf.searchapi.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
 import com.rf.searchapi.model.JokeResult;
 
 public class JokeServiceImpl implements JokeService {
 	
+	@Value("{api.joke.base}")
+	private String apiBase;
 	
 
 	@Override
 	public JokeResult getJokes(String firstName, String lastName) {
-		// TODO Auto-generated method stub
-		return null;
+		RestTemplate template = new RestTemplate();
+		return template.getForObject(apiBase, JokeResult.class);		
 	}
 
 }
