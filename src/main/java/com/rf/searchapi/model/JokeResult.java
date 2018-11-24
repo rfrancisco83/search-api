ded02185e17ecbb289f55d4461f0b4e4508489e2
r-francisco <rfrancisco@laughlin.com>
2018-11-24 14:57:15 -0600

    Requesting API, parameter cleanup, object changes for structure of json

--- src/main/java/com/rf/searchapi/model/JokeResult.java
+++ src/main/java/com/rf/searchapi/model/JokeResult.java
@@ -1,5 +1,19 @@
 package com.rf.searchapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class JokeResult {
+	
+	@JsonProperty("id")
+	public int id;
+	
+	@JsonProperty("type")
+	public String type;
+	
+	@JsonProperty("setup")
+	public String setup;
+	
+	@JsonProperty("punchline")
+	public String punchline;
 
 }
--- src/main/java/com/rf/searchapi/service/JokeServiceImpl.java
+++ src/main/java/com/rf/searchapi/service/JokeServiceImpl.java
@@ -1,10 +1,11 @@
 package com.rf.searchapi.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import com.rf.searchapi.model.JokeResultList;
 
-import com.rf.searchapi.model.JokeResult;
-
+@Service("jokeService")
 public class JokeServiceImpl implements JokeService {
 	
 	@Value("{api.joke.base}")
@@ -12,9 +13,9 @@ public class JokeServiceImpl implements JokeService {
 	
 
 	@Override
-	public JokeResult getJokes(String firstName, String lastName) {
+	public JokeResultList getJokes() {
 		RestTemplate template = new RestTemplate();
-		return template.getForObject(apiBase, JokeResult.class);		
+		return template.getForObject(apiBase, JokeResultList.class);		
 	}
 
 }
