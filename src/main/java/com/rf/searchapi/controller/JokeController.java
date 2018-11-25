b8783bf30fefce2417d2ea3459ee86905d87d9f2
r-francisco <rfrancisco@laughlin.com>
2018-11-25 12:24:28 -0600

    Allowing local React App

--- src/main/java/com/rf/searchapi/controller/JokeController.java
+++ src/main/java/com/rf/searchapi/controller/JokeController.java
@@ -1,6 +1,7 @@
 package com.rf.searchapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class JokeController {
 	@Autowired
 	JokeService jokeService;
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping("/jokes")
 	public JokeResultList getJokes() {
 		
