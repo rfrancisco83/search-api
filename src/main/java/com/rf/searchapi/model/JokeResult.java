f71d2618cf242a2270128674c064e55e4d0abdea
r-francisco <rfrancisco@laughlin.com>
2018-11-25 16:46:19 -0600

    equals and multiple calls.  make a unique set.

--- src/main/java/com/rf/searchapi/model/JokeResult.java
+++ src/main/java/com/rf/searchapi/model/JokeResult.java
@@ -4,6 +4,28 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class JokeResult {
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JokeResult other = (JokeResult) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 	@JsonProperty("id")
 	public int id;
 	
--- src/main/java/com/rf/searchapi/service/JokeServiceImpl.java
+++ src/main/java/com/rf/searchapi/service/JokeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.rf.searchapi.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,10 +23,17 @@ public class JokeServiceImpl implements JokeService {
 	@Override
 	public JokeResultList getJokes() {
 		RestTemplate template = new RestTemplate();
-		List<JokeResult> finalList = Arrays.asList(template.getForObject(apiBase, JokeResult[].class));
+		//api doesn't provide a customizable length, so let's just call twice and merge
+		List<JokeResult> list1 = Arrays.asList(template.getForObject(apiBase, JokeResult[].class));
+		List<JokeResult> list2 = Arrays.asList(template.getForObject(apiBase, JokeResult[].class));
+		
+		//JokeResult overrides equals() and hashcode().  Adding to set will eliminate dupes.
+		Set<JokeResult> setOfJokes = new HashSet<JokeResult>();
+		setOfJokes.addAll(list1);
+		setOfJokes.addAll(list2);
 		
 		JokeResultList allTheJokes = new JokeResultList();
-		allTheJokes.setResults(finalList);
+		allTheJokes.setResults(new ArrayList<JokeResult>(setOfJokes));
 		
 		return allTheJokes;		
 	}
